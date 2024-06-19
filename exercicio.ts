4a1eb7ebc11f7f59502316b8dbaa38382f5f6481
ramoncastro674 <tadeuramoncastro@gmail.com>
2024-06-19 14:02:23 -0300

    first commit

---
+++ exercicio.ts
@@ -0,0 +1,13 @@
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+function greet(name: string): string {
+    return "Olá " + name;
+}
+
+const result = multiply(3, 4);
+console.log(result);
+
+const greeting = greet("João");
+console.log(greeting);
